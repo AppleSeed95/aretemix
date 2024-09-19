9a463a77554cea3db6b4d7b7892c7eb457c075b6
AppleSeed <everlastingfriendship194@gmail.com>
2024-09-19 16:26:17 +0200

    fix: collector type

--- crates/artemis-core/src/collectors/block_collector.rs
+++ crates/artemis-core/src/collectors/block_collector.rs
@@ -1,32 +1,32 @@
-use crate::types::{Collector, CollectorStream};
-use anyhow::Result;
-use async_trait::async_trait;
-use ethers::{
-    prelude::Middleware,
-    providers::PubsubClient,
-    types::{H256, U64},
-};
-use std::sync::Arc;
-use tokio_stream::StreamExt;
+// use crate::types::{Collector, CollectorStream};
+// use anyhow::Result;
+// use async_trait::async_trait;
+// use ethers::{
+//     prelude::Middleware,
+//     providers::PubsubClient,
+//     types::{H256, U64},
+// };
+// use std::sync::Arc;
+// use tokio_stream::StreamExt;
 
 /// A collector that listens for new blocks, and generates a stream of
 /// [events](NewBlock) which contain the block number and hash.
-// pub struct BlockCollector<M> {
-//     provider: Arc<M>,
-// }
+pub struct BlockCollector<M> {
+    provider: Arc<M>,
+}
 
-// /// A new block event, containing the block number and hash.
-// #[derive(Debug, Clone)]
-// pub struct NewBlock {
-//     pub hash: H256,
-//     pub number: U64,
-// }
+/// A new block event, containing the block number and hash.
+#[derive(Debug, Clone)]
+pub struct NewBlock {
+    pub hash: H256,
+    pub number: U64,
+}
 
-// impl<M> BlockCollector<M> {
-//     pub fn new(provider: Arc<M>) -> Self {
-//         Self { provider }
-//     }
-// }
+impl<M> BlockCollector<M> {
+    pub fn new(provider: Arc<M>) -> Self {
+        Self { provider }
+    }
+}
 
 /// Implementation of the [Collector](Collector) trait for the [BlockCollector](BlockCollector).
 /// This implementation uses the [PubsubClient](PubsubClient) to subscribe to new blocks.
